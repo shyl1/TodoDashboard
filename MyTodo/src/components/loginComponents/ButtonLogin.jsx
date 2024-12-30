65e2ef4671b6366d0666c10f019eac8e4a091309
shyl <sohaylae615@gmail.com>
2024-12-30 19:29:42 +0200

    add Button component and style it

---
+++ MyTodo/src/components/loginComponents/ButtonLogin.jsx
@@ -0,0 +1,9 @@
+import styles from "../loginStyling/buttonStyling.module.css";
+
+export default function ButtonLogin() {
+  return (
+    <>
+      <button type='submit' className={styles.btn}>login</button>
+    </>
+  )
+}
--- MyTodo/src/components/loginComponents/Login.jsx
+++ MyTodo/src/components/loginComponents/Login.jsx
@@ -1,8 +1,11 @@
 // for styling 
-
 import styles from '../loginStyling/login.module.css';
+
+//importing form login components
 import Email from './Email';
 import Password from './Password';
+import ButtonLogin from './ButtonLogin';
+
 //import {useNavigate} from 'react-router-dom';
 export default function Login() {
  // handled
@@ -12,21 +15,20 @@ export default function Login() {
 
   //const navigate = useNavigate();
 
-  
-
   return (
     <div className={styles.Container}>
       <div className={styles.LoginContainer}>
         <form className={styles.form} onSubmit={handleSubmit}>
-          
         <div className={styles.emailConatiner}>
           <Email />
         </div>
-          <div className={styles.passwordContainer}>
-            <Password />
-            
-          </div>
-          <button type='submit'>login</button>
+        <div className={styles.passwordContainer}>
+          <Password />
+        </div>
+        <div className={styles.btnContainer}>
+          <ButtonLogin />
+        </div>
+          
         </form>
       </div>
     </div>
