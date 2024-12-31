31ce940febc5e5a5ac1879df46cd884341fe134b
shyl <sohaylae615@gmail.com>
2024-12-31 03:31:09 +0200

    routing login, dashboard

--- MyTodo/src/App.jsx
+++ MyTodo/src/App.jsx
@@ -1,7 +1,8 @@
 import Login from './components/loginComponents/Login.jsx';
-// import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 // import { AuthProvider } from './components/AuthenticationContext.jsx';
 import './App.css';
+import DashBoard from './components/Dashboardcomponents/DashBoard.jsx';
 
 
 
@@ -10,16 +11,13 @@ function App() {
   
   return (
     <>
-    {/* {<AuthProvider>
       <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />}/>
-
+        <Route path='/dashboard' element={<DashBoard />}/>
       </Routes>
       </BrowserRouter>
-    
-    </AuthProvider>} */}
-    <Login />
+
     </>
   )
 }
