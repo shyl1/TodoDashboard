7324d45fe42263c188729269ee39212503e58f03
shyl <sohaylae615@gmail.com>
2025-01-05 20:20:25 +0200

    installing react-icons

--- MyTodo/src/components/Dashboardcomponents/DashBoard.jsx
+++ MyTodo/src/components/Dashboardcomponents/DashBoard.jsx
@@ -1,11 +1,13 @@
-import { useContext } from "react";
-import { AuthContext } from "../AuthenticationContext";
+// import { useContext } from "react";
+//import { AuthContext } from "../AuthenticationContext";
 
 export default function DashBoard() {
-  const { isAuthenticated } = useContext(AuthContext);
-  console.log("isAuthenticated in Dashboard:", isAuthenticated); // Debugging
+  //const { isAuthenticated } = useContext(AuthContext);
+  //console.log("isAuthenticated in Dashboard:", isAuthenticated); // Debugging
 
   return (
-    <div>DashBoard</div>
+    <>
+      
+    </>
   )
 }
---
+++ MyTodo/src/components/Dashboardcomponents/SideBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function SideBar() {
+  return (
+    <>
+    <div className="text-container">
+      <h1>TODO</h1>
+    </div>
+    <div className="pages">
+      
+    </div>
+    </>
+  )
+}
