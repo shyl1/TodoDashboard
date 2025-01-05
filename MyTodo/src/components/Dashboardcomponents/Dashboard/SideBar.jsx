78c23d8fce263f44407e3ac8c545b96832260646
shyl <sohaylae615@gmail.com>
2025-01-05 20:50:15 +0200

    styling SideBar

--- MyTodo/src/components/Dashboardcomponents/Dashboard/SideBar.jsx
+++ MyTodo/src/components/Dashboardcomponents/Dashboard/SideBar.jsx
@@ -5,11 +5,11 @@ export default function SideBar() {
   return (
     <>
     <div className={styles.container}>
-      <div className="text-container">
-        <h1>TODO</h1>
+      <div className={styles.textContainer}>
+        <h1 className={styles.text}>TODO</h1>
       </div>
-      <div className="pages">
-      <h1><FaBorderAll />Dashboard</h1>  
+      <div className={styles.pages}>
+      <FaBorderAll size={30}/><h1>Dashboard</h1>  
       </div>
     </div>
     </>
